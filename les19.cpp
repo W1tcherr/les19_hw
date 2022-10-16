d1d032eb54936dc484e5a7779db3f7bdfe34a210
W1tcherr <frost4tt@gmail.com>
2022-10-16 21:02:22 +0300

    решение задачи

---
+++ les19.cpp
@@ -0,0 +1,22 @@
+﻿#include "les19.h"
+#include <vector>
+
+int main()
+{
+	std::vector<Animal*> animal;
+	animal.emplace_back(new Cat());
+	animal.emplace_back(new Dog());
+	animal.emplace_back(new Fox());
+
+	for (auto &el : animal)
+	{
+		el->Voice();
+	}
+	
+	while (animal.size())
+	{
+		delete animal[0];
+		animal.erase(animal.begin());
+	}
+	return 0;
+}
---
+++ les19.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include <iostream>
+
+class Animal
+{
+public:
+	virtual void Voice() {}
+
+	virtual ~Animal() {};
+};
+
+class Cat : public Animal
+{
+	~Cat() override
+	{
+		std::cout << "\n remove Cat" << std::endl;
+	}
+public:
+	void Voice() override
+	{
+		std::cout << "\n Cat say Myau!" << std::endl;
+	}
+};
+
+class Dog : public Animal
+{
+	~Dog() override
+	{
+		std::cout << "\n remove Dog" << std::endl;
+	}
+public:
+	void Voice() override
+	{
+		std::cout << "\n Dog say Woof!" << std::endl;
+	}
+};
+
+class Fox : public Animal
+{
+	~Fox() override
+	{
+		std::cout << "\n remove Fox" << std::endl;
+	}
+public:
+	void Voice() override
+	{
+		std::cout << "\n What Does the Fox Say?" << std::endl;
+	}
+};
